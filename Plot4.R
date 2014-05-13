c07ae80cb1d3ab34758b6a7f8653f910d721e25f
Akhil Kumar Ramasagaram <akhil.ramasagaram@gmail.com>
2014-05-13 10:54:40 +0530

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,3 +1,4 @@
+Sys.setlocale("LC_TIME", "C")
 # Let's load the dataset into R.
 data = read.table("household_power_consumption.txt", header=TRUE, sep = ";", na.strings = "?")
 
@@ -22,8 +23,8 @@ lines(data$DateTime, data$Sub_metering_3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, bty="n", col=c("black", "red", "blue")) # The end of adding the second plot.
 
 #Plot 3
-plot(data$DateTime, data$Voltage, type="l", xlab="datetime", ylab="Voltage")   # Adding the third plot.
+plot(data$DateTime, data$Voltage, type="l", xlab="datetime", ylab="Voltage")
 
 #Plot 4
-plot(data$DateTime, data$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")  # Adding the forth plot.
+plot(data$DateTime, data$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
 dev.off()
